638e4b819a225a4929e12b3c1f805190e17908ee
Utkarsh-Jain2199 <77992826+Utkarsh-Jain2199@users.noreply.github.com>
2023-05-29 04:55:27 +0530

    Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -285,115 +285,6 @@
 // }
 
 
-// import React, { useEffect, useState } from 'react';
-// import Card from '../components/Card';
-// import Footer from '../components/Footer';
-// import Navbar from '../components/Navbar';
-
-// export default function Home() {
-//   const [foodCat, setFoodCat] = useState([]);
-//   const [foodItems, setFoodItems] = useState([]);
-//   const [search, setSearch] = useState('');
-
-//   const loadFoodItems = async () => {
-//     let success = false;
-//     while (!success) {
-     
-//       try {     
-//         let response = await fetch("https://meal-express-backend.onrender.com/api/auth/foodData", {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json'
-//           }
-//         });
-
-//         if (response.ok) {
-//           response = await response.json();
-//           //console.log(response.length, "food and item");
-//           setFoodItems(response[0]);
-//           setFoodCat(response[1]);
-//           success = true
-//         } else {
-//           console.log("api failure",response)
-//         }
-
-
-//       } catch (error) {
-//         console.log(error, "error while calling api  ")
-//       }
-//     }
-//     if (!success) {
-//       console.log('Retrying API call...');
-//       await new Promise((resolve) => setTimeout(resolve, 1000));
-//     }
-//   }
-
-//   useEffect(() => {
-//     loadFoodItems();
-//   }, []);
-
-//   return (
-//     <div>
-//       <div>
-//         <Navbar />
-//       </div>
-//       <div>
-//         <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
-//           <div className="carousel-inner" id='carousel'>
-//             <div className="carousel-caption" style={{ zIndex: "9" }}>
-//               <div className="d-flex justify-content-center">
-//                 <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
-//                 <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>X</button>
-//               </div>
-//             </div>
-//             <div className="carousel-item active">
-//               <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
-//             </div>
-//             <div className="carousel-item">
-//               <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
-//             </div>
-//             <div className="carousel-item">
-//               <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
-//             </div>
-//           </div>
-//           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-//             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-//             <span className="visually-hidden">Previous</span>
-//           </button>
-//           <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-//             <span className="carousel-control-next-icon" aria-hidden="true"></span>
-//             <span className="visually-hidden">Next</span>
-//           </button>
-//         </div>
-//       </div>
-//       <div className='container'>
-//         {foodCat && foodCat.length !== 0 ? foodCat.map((data) => {
-//           return (
-//             <div className='row mb-3' key={data.id}>
-//               <div className='fs-3 m-3'>
-//                 {data.CategoryName}
-//               </div>
-//               <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left, rgb(0, 255, 137), rgb(0, 0, 0))" }} />
-//               {foodItems && foodItems.length !== 0 ? foodItems
-//                 .filter((items) => items.CategoryName === data.CategoryName && items.name.toLowerCase().includes(search.toLowerCase()))
-//                 .map(filterItems => {
-//                  // console.log(filterItems, "key")
-//                   return (
-//                     <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
-//                       <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems?.img} />
-//                     </div>
-//                   )
-//                 }) : <div>No Such Data</div>}
-//             </div>
-//           )
-//         }) : ""}
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-
 import React, { useEffect, useState } from 'react';
 import Card from '../components/Card';
 import Footer from '../components/Footer';
@@ -407,7 +298,8 @@ export default function Home() {
   const loadFoodItems = async () => {
     let success = false;
     while (!success) {
-      try {
+     
+      try {     
         let response = await fetch("https://meal-express-backend.onrender.com/api/auth/foodData", {
           method: 'POST',
           headers: {
@@ -417,22 +309,24 @@ export default function Home() {
 
         if (response.ok) {
           response = await response.json();
-          console.log(response.length, "food and item");
+          //console.log(response.length, "food and item");
           setFoodItems(response[0]);
           setFoodCat(response[1]);
-          success = true;
+          success = true
         } else {
-          console.log("API failure", response);
+          console.log("api failure",response)
         }
+
+
       } catch (error) {
-        console.log(error, "error while calling the API");
+        console.log(error, "error while calling api  ")
       }
     }
     if (!success) {
       console.log('Retrying API call...');
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
-  };
+  }
 
   useEffect(() => {
     loadFoodItems();
@@ -440,18 +334,26 @@ export default function Home() {
 
   return (
     <div>
-      <Navbar />
-      <div className="carousel-wrapper">
+      <div>
+        <Navbar />
+      </div>
+      <div>
         <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
-          <div className="carousel-inner">
+          <div className="carousel-inner" id='carousel'>
+            <div className="carousel-caption" style={{ zIndex: "9" }}>
+              <div className="d-flex justify-content-center">
+                <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
+                <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>X</button>
+              </div>
+            </div>
             <div className="carousel-item active">
-              <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" alt="Burger" />
+              <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
             </div>
             <div className="carousel-item">
-              <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100" alt="Pastry" />
+              <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
             </div>
             <div className="carousel-item">
-              <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100" alt="Barbeque" />
+              <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
             </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
@@ -462,52 +364,37 @@ export default function Home() {
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Next</span>
           </button>
-          <div className="carousel-search">
-            <input
-              className="form-control me-2 bg-white text-dark"
-              type="search"
-              placeholder="Search here..."
-              aria-label="Search"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <button className="btn btn-danger" onClick={() => setSearch('')}>
-              X
-            </button>
-          </div>
         </div>
       </div>
-      <div className="container">
-        {foodCat.length !== 0 ? (
-          foodCat.map((data) => (
-            <div className="row mb-3" key={data.id}>
-              <div className="col-12">
-                <div className="category-heading fs-3">{data.CategoryName}</div>
-                <hr className="hr-success" />
+      <div className='container'>
+        {foodCat && foodCat.length !== 0 ? foodCat.map((data) => {
+          return (
+            <div className='row mb-3' key={data.id}>
+              <div className='fs-3 m-3'>
+                {data.CategoryName}
               </div>
-              {foodItems.length !== 0 ? (
-                foodItems
-                  .filter((item) => item.CategoryName === data.CategoryName && item.name.toLowerCase().includes(search.toLowerCase()))
-                  .map((filterItem) => (
-                    <div className="col-12 col-md-6 col-lg-3" key={filterItem._id}>
-                      <Card foodName={filterItem.name} item={filterItem} options={filterItem.options[0]} ImgSrc={filterItem?.img} />
+              <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left, rgb(0, 255, 137), rgb(0, 0, 0))" }} />
+              {foodItems && foodItems.length !== 0 ? foodItems
+                .filter((items) => items.CategoryName === data.CategoryName && items.name.toLowerCase().includes(search.toLowerCase()))
+                .map(filterItems => {
+                 // console.log(filterItems, "key")
+                  return (
+                    <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
+                      <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems?.img} />
                     </div>
-                  ))
-              ) : (
-                <div className="col-12">
-                  <div className="no-data">No data available</div>
-                </div>
-              )}
+                  )
+                }) : <div>No Such Data</div>}
             </div>
-          ))
-        ) : (
-          <div className="no-data">No categories found</div>
-        )}
+          )
+        }) : ""}
       </div>
       <Footer />
     </div>
   );
 }
+
+
+
 
 
 
