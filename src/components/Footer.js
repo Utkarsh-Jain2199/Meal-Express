eed0e1e53afd0db49666c850d3e4aaaa13e8d754
Utkarsh-Jain2199 <77992826+Utkarsh-Jain2199@users.noreply.github.com>
2024-08-19 04:52:13 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@
 //         </a>
 //         <span className="text-muted">© 2023 <i>Meal Express</i>, Inc</span>
 //       </div>
-  
+
 //       <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
 //         <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use ></use></svg></a></li>
 //         <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use ></use></svg></a></li>
@@ -21,33 +21,72 @@
 //   )
 // }
 
+// import React from 'react';
+
+// export default function Footer() {
+//   return (
+//     <footer className="bg-light py-4">
+//       <div className="container">
+//         <div className="row">
+//           <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+//             <a href="/" className="text-muted text-decoration-none">
+//               <h5>Meal Express</h5>
+//             </a>
+//             <p className="text-muted">© 2023 Meal Express, Inc</p>
+//           </div>
+//           <div className="col-md-6 d-flex justify-content-center justify-content-md-end">
+//             <ul className="list-inline">
+//               <li className="list-inline-item me-3">
+//                 <a className="text-muted" href="/">
+//                   <i className="bi bi-facebook"></i>
+//                 </a>
+//               </li>
+//               <li className="list-inline-item me-3">
+//                 <a className="text-muted" href="/">
+//                   <i className="bi bi-twitter"></i>
+//                 </a>
+//               </li>
+//               <li className="list-inline-item">
+//                 <a className="text-muted" href="/">
+//                   <i className="bi bi-instagram"></i>
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// }
+
 import React from 'react';
+import './styles/Footer.css'; // New CSS file for additional styling
 
 export default function Footer() {
   return (
-    <footer className="bg-light py-4">
+    <footer className="bg-dark text-light py-5">
       <div className="container">
-        <div className="row">
+        <div className="row align-items-center">
           <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-            <a href="/" className="text-muted text-decoration-none">
-              <h5>Meal Express</h5>
+            <a href="/" className="text-decoration-none">
+              <h5 className="footer-brand">Meal Express</h5>
             </a>
-            <p className="text-muted">© 2023 Meal Express, Inc</p>
+            <p className="text-muted mb-0">© 2024 Meal Express, Inc. All rights reserved.</p>
           </div>
           <div className="col-md-6 d-flex justify-content-center justify-content-md-end">
-            <ul className="list-inline">
+            <ul className="list-inline mb-0">
               <li className="list-inline-item me-3">
-                <a className="text-muted" href="/">
+                <a className="text-light social-icon" href="/">
                   <i className="bi bi-facebook"></i>
                 </a>
               </li>
               <li className="list-inline-item me-3">
-                <a className="text-muted" href="/">
+                <a className="text-light social-icon" href="/">
                   <i className="bi bi-twitter"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a className="text-muted" href="/">
+                <a className="text-light social-icon" href="/">
                   <i className="bi bi-instagram"></i>
                 </a>
               </li>
