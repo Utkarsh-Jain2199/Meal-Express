6232cb546c1c7def7c4ad7344e0e035e75e20416
Utkarsh-Jain2199 <77992826+Utkarsh-Jain2199@users.noreply.github.com>
2024-08-19 04:37:12 +0530

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-
+import '../components/styles/MyOrder.css';
 
 export default function MyOrder() {
     const [orderData, setOrderData] = useState(null);
